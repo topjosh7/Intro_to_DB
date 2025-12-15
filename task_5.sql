6a0d9117ea32f0bb47926857f5489163e8d047cf
topjosh7 <topjosh7@gmail.com>
2025-12-15 01:28:21 +0100

    Add SQL script to insert a customer record
    Insert a new customer record into the customer table.

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+-- task_5.sql
+
+-- Insert a single row into the customer table.
+-- The database name (alx_book_store) will be passed as an argument
+-- when executing the mysql command.
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
